ebeaf13d9c19ddf91d309a8f98eda02088048b35
Joshua Chan <joshualchan@gmail.com>
2020-04-20 14:23:20 -0700

    flix part2 completed (added tab bar controller and collection view)

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Joshua Chan on 4/18/20.
+//  Copyright © 2020 Joshua Chan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
